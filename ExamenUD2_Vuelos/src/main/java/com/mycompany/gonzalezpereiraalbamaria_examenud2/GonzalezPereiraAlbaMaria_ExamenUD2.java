1844978dd647b34080bd3573fe8a5cfb3f034222
Alba <98285852+AlbaGonzalezPereira@users.noreply.github.com>
2024-12-11 23:58:09 +0100

    add examen

---
+++ ExamenUD2_Vuelos/src/main/java/com/mycompany/gonzalezpereiraalbamaria_examenud2/GonzalezPereiraAlbaMaria_ExamenUD2.java
@@ -0,0 +1,16 @@
+package com.mycompany.gonzalezpereiraalbamaria_examenud2;
+
+import com.mycompany.gonzalezpereiraalbamaria_examenud2.GonzalezAlba_Vuelos;
+
+
+/**
+ * Clase main
+ * @author alba gonzález
+ */
+public class GonzalezPereiraAlbaMaria_ExamenUD2 {
+
+    public static void main(String[] args) {
+        GonzalezAlba_Vuelos vuelos = new GonzalezAlba_Vuelos();
+        vuelos.setVisible(true);
+    }
+}
