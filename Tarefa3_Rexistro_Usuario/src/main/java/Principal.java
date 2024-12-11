f4e0b19d2c4b48b1ecc646070b3caff3eeed0021
Alba <98285852+AlbaGonzalezPereira@users.noreply.github.com>
2024-12-11 12:31:50 +0100

    add tarefas3 e 4

---
+++ Tarefa3_Rexistro_Usuario/src/main/java/Principal.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author alba_
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Tarefa4/src/main/java/Principal.java
@@ -0,0 +1,16 @@
+
+/**
+ * Clase principal
+ * @author alba_
+ */
+public class Principal {
+    public static void main(String[] args) {
+        //chamamos ao método inicializar
+        AMGP_GridBagLayout gbl = new AMGP_GridBagLayout();
+        gbl.inicializar();
+      
+        AMGP_BoxLayout bl = new AMGP_BoxLayout();
+        bl.inicializar();
+    }
+    
+}
