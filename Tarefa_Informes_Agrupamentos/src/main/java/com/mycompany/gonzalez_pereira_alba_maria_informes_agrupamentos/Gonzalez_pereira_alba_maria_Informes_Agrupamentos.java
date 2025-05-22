5fff8279bebdad21692886d7a411ca14ce38ab06
Alba <98285852+AlbaGonzalezPereira@users.noreply.github.com>
2025-05-22 08:56:55 +0200

    add tarefas

---
+++ Tarefa_Informes_Agrupamentos/src/main/java/com/mycompany/gonzalez_pereira_alba_maria_informes_agrupamentos/Gonzalez_pereira_alba_maria_Informes_Agrupamentos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gonzalez_pereira_alba_maria_informes_agrupamentos;
+
+/**
+ *
+ * @author alba_
+ */
+public class Gonzalez_pereira_alba_maria_Informes_Agrupamentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
